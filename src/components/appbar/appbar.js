f71e1c577537a83c8df492423059868b667a9974
Alan He <qianghe421@gmail.com>
2025-02-06 16:10:07 +0800

    Add copy to clipboard functionality to AppBar

--- src/components/appbar/appbar.js
+++ src/components/appbar/appbar.js
@@ -1,14 +1,44 @@
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import ContentContentCopy from 'material-ui/svg-icons/content/content-copy';
 import React from 'react';
 import { connect } from 'react-redux';
-import AppBar from 'material-ui/AppBar';
 
 const Bar = props => {
+  const handleCopy = () => {
+    const mediumElement = document.querySelector('#medium');
+    // 创建一个范围
+    const range = document.createRange();
+    range.selectNode(mediumElement);
+    
+    // 清除当前选择
+    window.getSelection().removeAllRanges();
+    // 选择目标内容
+    window.getSelection().addRange(range);
+    
+    // 执行复制命令
+    try {
+      document.execCommand('copy');
+      alert('已复制到剪贴板！');
+    } catch (err) {
+      console.error('复制失败:', err);
+    }
+    
+    // 清除选择
+    window.getSelection().removeAllRanges();
+  };
+
   return (
     <div>
       <AppBar
         title="Markdown to Medium"
         showMenuIconButton={false}
-        />
+        iconElementRight={
+          <IconButton onClick={handleCopy} tooltip="复制到剪贴板">
+            <ContentContentCopy color="#fff" />
+          </IconButton>
+        }
+      />
     </div>
   );
 };
